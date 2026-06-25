932573158ac2723fc0a66911bf744991d7a2c794
Meraj <mbd.banu@gmail.com>
2026-06-25 14:45:40 +0530

    completed the backend apis of user authentication (cookie based auth)

---
+++ test-project-backend/src/application/User.ts
@@ -0,0 +1,82 @@
+import { Request,Response,NextFunction } from "express";
+import User from "../infrastructure/db/entities/User";
+import bcrypt from "bcrypt";
+import {createToken} from "../JWT";   
+
+const registerUser = async(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    try {
+        const { name, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({name, password: hashedPassword});
+        await newUser.save();
+        res.status(201).json({ message: "User registered successfully" });
+      
+       
+    } catch (error) {
+        next(error);
+    }
+};
+
+const loginUser = async(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {  
+    try {
+        const {name, password} = req.body;
+        const user = await User.findOne({name:name});
+
+        if(!user){
+            return res.status(404).json({message: "User not found"});
+        }
+        
+
+        const dbPassword = user.password;
+        const isPasswordtrue = await bcrypt.compare(password,dbPassword);
+        if(!isPasswordtrue){
+            return res.status(401).json({message: "Username or password is incorrect please try again!"});
+        }
+
+        else{
+            const accessToken = createToken(user);
+            res.cookie("access-Token", accessToken, {
+                maxAge: 24 * 60 * 60 * 1000, 
+            })
+            res.status(200).json({message: "User logged in successfully"});
+         }
+        
+    } catch (error) {
+        next(error);
+    }
+};
+
+const logoutUser = async(
+    req:Request,
+    res:Response,
+    next:NextFunction
+) => {
+    try{
+        res.clearCookie("access-Token");
+        res.status(200).json({message: "User logged out successfully"});
+    } catch (error) {
+        next(error);
+    }
+};
+
+const profile = async (
+    req:Request,
+    res:Response,
+    next:NextFunction
+) => {
+    try{
+        res.status(200).json({message: "Welcome to the profile "})
+    }catch(error){
+        next(error)
+    }
+};
+
+export { registerUser, loginUser, logoutUser,profile };
--- test-project-backend/src/index.ts
+++ test-project-backend/src/index.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import connectDB from './infrastructure/db'
 import 'dotenv/config';
+import UserRouter from './api/User';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/users', UserRouter);
 
 connectDB();
 
